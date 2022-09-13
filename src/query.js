b62bb4852ad1e13ba4d124a293221a895fddf4a6
poqopo <block.poqopo@gmail.com>
2022-09-13 18:48:58 +0900

    migrate local to aws

--- src/query.js
+++ src/query.js
@@ -2,10 +2,16 @@ import pg from "pg";
 
 export function connection() {
   const config = {
-    host: "ls-b24c2551f8052c71c0c44d92efc079203b9c0789.c9wknlpud9ss.us-east-1.rds.amazonaws.com",
-    user: "test",
-    password: "12341234",
-    database: "postgres",
+    // host: "ls-b24c2551f8052c71c0c44d92efc079203b9c0789.c9wknlpud9ss.us-east-1.rds.amazonaws.com",
+    // user: "test",
+    // password: "12341234",
+    // database: "postgres",
+    // port: 5432,
+
+    host: 'localhost',
+    user: 'test',
+    password: 'test',
+    database: 'postgres',
     port: 5432,
   };
 
