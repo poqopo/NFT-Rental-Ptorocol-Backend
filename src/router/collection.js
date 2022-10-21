b95350d68b7d2502c22aefc2f76b60d327ac21a1
박상준 <sangjunpark@bagsangjun-ui-MacBookPro.local>
2022-10-21 18:25:06 +0900

    Add query string

--- src/router/collection.js
+++ src/router/collection.js
@@ -5,17 +5,17 @@ const router = express.Router();
 router.get("/collections", async (req, res) => {
   const client = await connection();
   const result = await client.query(
-    `SELECT * FROM collection`
+    `SELECT * FROM collection LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   );
-
+// 
   res.send(result.rows);
   client.end();
 });
 
-router.get("/nfts/:collectionAddress", async (req, res) => {
+router.get("/nfts/:collectionAddress/", async (req, res) => {
   const client = await connection();
   const result = await client.query(
-    `SELECT * FROM nft WHERE nft.collection_address = '${req.params.collectionAddress}' ORDER BY token_id ASC`
+    `SELECT * FROM nft WHERE nft.collection_address = '${req.params.collectionAddress}' ORDER BY token_id ASC LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   );
 
   res.send(result.rows);
@@ -27,7 +27,7 @@ router.get("/nfts/:collectionAddress/view/listed", async (req, res) => {
   const result = await client.query(
     `SELECT * FROM nft 
         INNER JOIN rentinfo ON nft.collection_address = rentinfo.collection_address and nft.token_id = rentinfo.token_id
-        WHERE nft.collection_address = '${req.params.collectionAddress}' and rentinfo.renter_address is NULL`
+        WHERE nft.collection_address = '${req.params.collectionAddress}' and rentinfo.renter_address is NULL LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   );
 
   res.send(result.rows);
@@ -39,7 +39,7 @@ router.get("/nfts/:collectionAddress/view/rented", async (req, res) => {
   const result = await client.query(
     `SELECT * FROM nft 
         INNER JOIN rentinfo ON nft.collection_address = rentinfo.collection_address and nft.token_id = rentinfo.token_id
-        WHERE nft.collection_address = '${req.params.collectionAddress}' and rentinfo.renter_address is NOT NULL`
+        WHERE nft.collection_address = '${req.params.collectionAddress}' and rentinfo.renter_address is NOT NULL LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   );
 
   res.send(result.rows);
@@ -52,7 +52,7 @@ router.get("/nfts/:collectionAddress/sort/:sort", async (req, res) => {
       `SELECT * FROM nft 
       INNER JOIN rentinfo ON nft.collection_address = rentinfo.collection_address and nft.token_id = rentinfo.token_id
       WHERE nft.collection_address = '${req.params.collectionAddress}'
-      ORDER BY rentinfo.${req.params.sort} ASC`
+      ORDER BY rentinfo.${req.params.sort} ASC LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   )
 
   res.send(result.rows);
@@ -64,7 +64,7 @@ router.get("/nfts/:collectionAddress/view/listed/sort/:sort", async (req, res) =
       `SELECT * FROM nft 
       INNER JOIN rentinfo ON nft.collection_address = rentinfo.collection_address and nft.token_id = rentinfo.token_id
       WHERE nft.collection_address = '${req.params.collectionAddress}' and rentinfo.renter_address is NULL
-      ORDER BY rentinfo.${req.params.sort} ASC`
+      ORDER BY rentinfo.${req.params.sort} ASC LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   )
 
   res.send(result.rows);
@@ -76,7 +76,7 @@ router.get("/nfts/:collectionAddress/view/rented/sort/:sort", async (req, res) =
       `SELECT * FROM nft 
       INNER JOIN rentinfo ON nft.collection_address = rentinfo.collection_address and nft.token_id = rentinfo.token_id
       WHERE nft.collection_address = '${req.params.collectionAddress}' and rentinfo.renter_address is NOT NULL
-      ORDER BY rentinfo.${req.params.sort} ASC`
+      ORDER BY rentinfo.${req.params.sort} ASC LIMIT ${req.query.size} offset ${(parseInt(req.query.page) -1) * req.query.size}`
   )
 
   res.send(result.rows);
